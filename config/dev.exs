34a3c80ca6333dd789a1df567bc7bbc8d50b5be7
Dave Townsend <townsend@patriot.net>
2016-06-25 10:50:44 -0400

    Set dev+test db information

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :sbg_inv, SbgInv.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+#  username: "postgres",
+#  password: "postgres",
   database: "sbg_inv_dev",
   hostname: "localhost",
   pool_size: 10
