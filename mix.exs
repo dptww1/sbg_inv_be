108ff0b08493bc016b939b268706ba2338276b5f
Dave Townsend <townsend@patriot.net>
2016-07-05 21:40:01 -0400

    Update Ecto version in case it matters for the failing test

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule SbgInv.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.1.6"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
+     {:phoenix_ecto, "~> 2.0.2"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
