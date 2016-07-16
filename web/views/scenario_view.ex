d6fedf436a120c6d254fa09e3911410128a9c636
Dave Townsend <townsend@patriot.net>
2016-07-15 20:21:09 -0400

    Scenario Resources returned in a typed map.
    A little more convenient for the client.

--- web/views/scenario_view.ex
+++ web/views/scenario_view.ex
@@ -2,7 +2,7 @@ defmodule SbgInv.ScenarioView do
   use SbgInv.Web, :view
 
   def render("index.json", %{scenarios: scenarios}) do
-    %{data: render_many(scenarios, SbgInv.ScenarioView, "scenario.json")}
+    %{data: render_many(scenarios, __MODULE__, "scenario.json")}
   end
 
   def render("show.json", %{scenario: scenario}) do
@@ -17,12 +17,22 @@ defmodule SbgInv.ScenarioView do
       date_year: scenario.date_year,
       is_canonical: scenario.is_canonical,
       size: scenario.size,
-      scenario_resources: render_many(scenario.scenario_resources, __MODULE__, "scenario_resource.json", as: :scenario_resource),
+      scenario_resources: render_one(massage_resources(scenario.scenario_resources), __MODULE__, "scenario_resources.json"),
       scenario_factions: render_many(scenario.scenario_factions, __MODULE__, "scenario_faction.json", as: :scenario_faction)
      }
   end
 
-  def render("scenario_resource.json", %{scenario_resource: resource}) do
+  def render("scenario_resources.json", %{scenario: scenario_resources}) do
+    %{
+      source:           render_many(scenario_resources.source,           __MODULE__, "scenario_resource.json"),
+      video_replay:     render_many(scenario_resources.video_replay,     __MODULE__, "scenario_resource.json"),
+      web_replay:       render_many(scenario_resources.web_replay,       __MODULE__, "scenario_resource.json"),
+      terrain_building: render_many(scenario_resources.terrain_building, __MODULE__, "scenario_resource.json"),
+      podcast:          render_many(scenario_resources.podcast,          __MODULE__, "scenario_resource.json")
+    }
+  end
+
+  def render("scenario_resource.json", %{scenario: resource}) do
     %{
       resource_type: resource.resource_type,
       book: resource.book,
@@ -40,6 +50,16 @@ defmodule SbgInv.ScenarioView do
       suggested_points: faction.suggested_points,
       actual_points: faction.actual_points,
       sort_order: faction.sort_order
+    }
+  end
+
+  defp massage_resources(scenario_resources) do
+    %{
+      source: Enum.filter(scenario_resources, &(&1.resource_type == :source)),
+      video_replay: Enum.filter(scenario_resources, fn r -> r.resource_type == :video_replay end),
+      web_replay: Enum.filter(scenario_resources, fn r -> r.resource_type == :web_replay end),
+      terrain_building: Enum.filter(scenario_resources, fn r -> r.resource_type == :terrain_building end),
+      podcast: Enum.filter(scenario_resources, fn r -> r.resource_type == :podcast end),
     }
   end
 end
