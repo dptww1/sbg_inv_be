2afd9a0ddb3a254772e9628b57e12324150999d2
Dave Townsend <townsend@patriot.net>
2016-08-09 18:48:00 -0400

    Removed leftover debugging code

--- web/views/scenario_faction_figure_view.ex
+++ web/views/scenario_faction_figure_view.ex
@@ -2,10 +2,6 @@ defmodule SbgInv.ScenarioFactionFigureView do
   use SbgInv.Web, :view
 
   def render("scenario_faction_figure.json", %{scenario_faction_figure: figure}) do
-    IO.inspect "#####"
-    IO.inspect figure
-    IO.inspect "#####"
-
     user_figure = if(((length figure.figure.user_figure) > 0), do: hd(figure.figure.user_figure), else: %SbgInv.UserFigure{ owned: 0, painted: 0})
 
     %{
