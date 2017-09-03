fbf3c7ce78cebde5970efd5b0b9c3685d574591e
Dave Townsend <townsend@patriot.net>
2017-09-02 21:58:53 -0400

    Added test to verify save works as well as update

--- test/task/recalc_user_scenario_task_test.exs
+++ test/task/recalc_user_scenario_task_test.exs
@@ -5,7 +5,22 @@ defmodule SbgInv.RecalcUserScenarioTaskTest do
   alias SbgInv.TestHelper
   alias SbgInv.Web.{RecalcUserScenarioTask, User, UserScenario}
 
-  test "UserScenario rollup uses only data from correct user", %{conn: conn} do
+  test "UserScenario rollup can be created", %{conn: conn} do
+    %{conn: _conn, const_data: const_data} = TestHelper.set_up_std_scenario(conn, :user1)
+
+    user = Repo.get_by!(User, email: "abc@def.com")
+
+    Repo.delete_all(UserScenario)
+
+    RecalcUserScenarioTask.do_task(user.id)
+
+    user_scenario = Repo.get_by!(UserScenario, %{scenario_id: Map.get(const_data, "id"), user_id: user.id})
+    assert 7 == user_scenario.owned
+    assert 3 == user_scenario.painted
+
+  end
+
+  test "Existing UserScenario rollup can be updated", %{conn: conn} do
     %{conn: _conn, const_data: const_data} = TestHelper.set_up_std_scenario(conn, :user1)
 
     user = Repo.get_by!(User, email: "abc@def.com")
