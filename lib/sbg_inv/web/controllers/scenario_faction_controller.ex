fb8ba26bfaf7d049a2dc261104273396d718cfdf
Dave Townsend <townsend@patriot.net>
2019-02-06 19:00:25 -0500

    Fix compiler warnings

--- lib/sbg_inv/web/controllers/scenario_faction_controller.ex
+++ lib/sbg_inv/web/controllers/scenario_faction_controller.ex
@@ -2,9 +2,7 @@ defmodule SbgInv.Web.ScenarioFactionController do
 
   use SbgInv.Web, :controller
 
-  import Ecto.Query
-
-  alias SbgInv.Web.{Authentication, Role, Scenario, ScenarioFaction}
+  alias SbgInv.Web.{Authentication, Scenario, ScenarioFaction}
 
   def update(conn, %{"id" => id, "scenario_faction" => params}) do
     conn = Authentication.admin_required(conn)
@@ -18,7 +16,7 @@ defmodule SbgInv.Web.ScenarioFactionController do
       changeset = ScenarioFaction.changeset(scenario_faction, params)
 
       case Repo.update(changeset) do
-        {:ok, role} ->
+        {:ok, _} ->
           scenario_faction = Repo.get(ScenarioFaction, id)
                              |> Repo.preload([roles: :figures])
           render(conn, "faction_detail.json", %{scenario_faction: scenario_faction})
