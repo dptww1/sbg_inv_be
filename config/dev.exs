82b80a28153e8b51b5f49568d4de04d81466c5c3
Dave Townsend <townsend@patriot.net>
2019-03-13 20:32:22 -0400

    No more brunch, on the back end at least.

--- config/dev.exs
+++ config/dev.exs
@@ -10,8 +10,7 @@ config :sbg_inv, SbgInv.Web.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin", cd: Path.expand("../assets", __DIR__)]]
+  check_origin: false
 
 # Watch static and templates for browser reloading.
 config :sbg_inv, SbgInv.Web.Endpoint,
