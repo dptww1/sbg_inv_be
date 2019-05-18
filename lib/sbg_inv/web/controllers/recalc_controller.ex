1520d6d6ae5d25305e0a0210be728b7c8e41f54f
Dave Townsend <townsend@patriot.net>
2019-05-18 09:42:28 -0400

    Recalc Controller can recalc arbitrary user

--- lib/sbg_inv/web/controllers/recalc_controller.ex
+++ lib/sbg_inv/web/controllers/recalc_controller.ex
@@ -2,9 +2,19 @@ defmodule SbgInv.Web.RecalcController do
 
   use SbgInv.Web, :controller
 
-  def index(conn, _params) do
-    Task.start_link(fn -> SbgInv.Web.RecalcUserScenarioTask.do_task(1) end)   # TODO get user id from conn
+  def index(conn, params) do
+    IO.puts "----params"
+    IO.inspect params
+    IO.puts "----"
 
-    text conn, "Starting recalc task at " <> NaiveDateTime.to_string(NaiveDateTime.utc_now())
+    user_id = Map.get(params, "uid", "1")
+              |> String.to_integer
+
+    Task.start_link(fn -> SbgInv.Web.RecalcUserScenarioTask.do_task(user_id) end)
+
+    text conn, "Starting recalc task at " <>
+               NaiveDateTime.to_string(NaiveDateTime.utc_now()) <>
+               " for user " <>
+               Integer.to_string(user_id)
   end
 end
