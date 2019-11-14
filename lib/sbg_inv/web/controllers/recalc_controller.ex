c384862009b201672e6226ea1f0b6a3f55b4bbb7
Dave Townsend <townsend@patriot.net>
2019-11-13 21:53:44 -0500

    Lobotomize recalc_controller

--- lib/sbg_inv/web/controllers/recalc_controller.ex
+++ lib/sbg_inv/web/controllers/recalc_controller.ex
@@ -3,18 +3,19 @@ defmodule SbgInv.Web.RecalcController do
   use SbgInv.Web, :controller
 
   def index(conn, params) do
-    IO.puts "----params"
-    IO.inspect params
-    IO.puts "----"
-
-    user_id = Map.get(params, "uid", "1")
-              |> String.to_integer
-
-    Task.start_link(fn -> SbgInv.Web.RecalcUserScenarioTask.do_task(user_id) end)
-
-    text conn, "Starting recalc task at " <>
-               NaiveDateTime.to_string(NaiveDateTime.utc_now()) <>
-               " for user " <>
-               Integer.to_string(user_id)
+    put_status(conn, :unauthorized)
+#    IO.puts "----params"
+#    IO.inspect params
+#    IO.puts "----"
+#
+#    user_id = Map.get(params, "uid", "1")
+#              |> String.to_integer
+#
+#    Task.start_link(fn -> SbgInv.Web.RecalcUserScenarioTask.do_task(user_id) end)
+#
+#    text conn, "Starting recalc task at " <>
+#               NaiveDateTime.to_string(NaiveDateTime.utc_now()) <>
+#               " for user " <>
+#               Integer.to_string(user_id)
   end
 end
