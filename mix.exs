fe4d787e48475d525b6a3d8cac65cf543cd76aed
Dave Townsend <townsend@patriot.net>
2024-01-09 21:49:35 -0500

    Upgrade to Elixir 1.15.x

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SbgInv.Mixfile do
   def project do
     [app: :sbg_inv,
      version: "0.0.1",
-     elixir: "1.14.5",
+     elixir: "1.15.7",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
