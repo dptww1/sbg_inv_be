cdb920793dee013ae4f22359a4e0273f7d8c8589
Dave Townsend <dave@davetownsend.org>
2025-08-02 21:56:59 -0400

    Style changes to better match other controllers

--- lib/sbg_inv/web/controllers/character_controller.ex
+++ lib/sbg_inv/web/controllers/character_controller.ex
@@ -8,59 +8,57 @@ defmodule SbgInv.Web.CharacterController do
 
   def show(conn, %{"id" => id}) do
     with_admin_user conn do
-      with {:ok, char} <- load_character(id)
-      do
+      if char = _load_character(id) do
         render(conn, "character.json", character: char)
       else
-        _ -> send_resp(conn, :unprocessable_entity, "")
+        send_resp(conn, :unprocessable_entity, "")
       end
     end
   end
 
   def create(conn, %{"character" => params}) do
     with_admin_user conn do
-      update_or_create(conn, %Character{}, params)
+      _create_or_update(conn, %Character{}, params)
     end
   end
 
   def update(conn, %{"id" => id, "character" => params}) do
     with_admin_user conn do
-      {:ok, char} = load_character(id)
-      update_or_create(conn, char, params)
+      char = _load_character(id)
+      _create_or_update(conn, char, params)
     end
   end
 
-  defp update_or_create(conn, nil, _), do: send_resp(conn, :unprocessable_entity, "")
-  defp update_or_create(conn, char, params) do
+  defp _create_or_update(conn, nil, _), do: send_resp(conn, :unprocessable_entity, "")
+  defp _create_or_update(conn, char, params) do
     success_status = if char.id, do: :ok, else: :created
 
     params = params
     |> Map.put("rules", BookUtils.add_book_refs(params["rules"]))
     |> Map.put("resources", BookUtils.add_book_refs(params["resources"]))
 
-    with {:ok, char} <- Repo.insert_or_update(Character.changeset(char, params)),
-         {:ok, char} <- load_character(char.id)
-    do
-      conn
-      |> put_status(success_status)
-      |> render("character.json", character: char)
-    else
-      _ -> send_resp(conn, :unprocessable_entity, "")
+    changeset = Character.changeset(char, params)
+
+    case Repo.insert_or_update(changeset) do
+      {:ok, character} ->
+        conn = put_status(conn, success_status)
+        character = _load_character(character.id)
+        render(conn, "character.json", character: character)
+
+      {:error, changeset} ->
+        IO.inspect changeset
+        conn
+        |> put_status(:unprocessable_entity)
+        |> put_view(SbgInv.Web.ChangesetView)
+        |> render("error.json", changeset: changeset)
     end
   end
 
-  defp load_character(id) do
-    char =
-      Character.query_by_id(id)
-      |> Character.with_figures
-      |> Character.with_resources
-      |> Character.with_rules
-      |> Repo.one
-
-    if char do
-      {:ok, char}
-    else
-      {:error}
-    end
+  defp _load_character(id) do
+    Character.query_by_id(id)
+    |> Character.with_figures
+    |> Character.with_resources
+    |> Character.with_rules
+    |> Repo.one
   end
 end
